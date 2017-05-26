593c0fa2926fb88c9df73be43330b1411d6a0656
dskuzm <dskuzm@gmail.com>
2017-05-26 04:30:18 +0300

    создал блок footer

--- build/js/script.js
+++ build/js/script.js
@@ -16,7 +16,7 @@ $(function () {
     $('.burger-icon').click(function () {
             $('body').toggleClass('nav-open');
         })
-        , $('.nav a').click(function () {
+        , $('nav a').click(function () {
             $('body').removeClass('nav-open');
         });
 });
